200cb13204e23bb1bb0651f9fa16114baa87ed55
Mosarof Hossain <mosarof.hossain@semyou.com>
2022-11-17 20:26:52 +0600

    added if

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-console.log("meshu 123 - form");
+console.log("1");
+
+function goToPage(pageUrl) {
+  if ($(".selected").length > 0) {
+
+    location.href = pageUrl
+  }
+}
+
+function goToQuiz() {
+  if ($("input").val().trim() !== "") {
+    //localStorage.getItem("save")
+    localStorage.clear();
+    location.href = 'quiz1.html';
+  }
+}
+
 $("body").on("click", ".quiz-page-answer", function () {
   $(this).toggleClass("selected")
 })
