872965901e878f4c456013f530626da622239e30
Mosarof Hossain <meshu@live.com>
2022-11-18 11:27:14 +0600

    added confetti, result calculation, enter on name, responsive

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ console.log("1");
 
 function goToPage(pageUrl) {
   if ($(".selected").length > 0) {
+    const a = $(".quiz-page-answer:eq(0)").is(".selected") ? "A" : "";
+    const b = $(".quiz-page-answer:eq(1)").is(".selected") ? "B" : "";
+    const c = $(".quiz-page-answer:eq(2)").is(".selected") ? "C" : "";
+    const d = $(".quiz-page-answer:eq(3)").is(".selected") ? "D" : "";
 
+    quizNo = location.href.split("/").pop()[4];
+    localStorage.setItem(quizNo, a + b + c + d);
     location.href = pageUrl
   }
 }
@@ -13,6 +19,14 @@ function goToQuiz() {
     localStorage.clear();
     location.href = 'quiz1.html';
   }
+}
+
+function goToQuizByPressingEnter(e) {
+  //console.log(e);
+  if (e.which === 13) {
+    goToQuiz()
+  }
+  
 }
 
 $("body").on("click", ".quiz-page-answer", function () {
